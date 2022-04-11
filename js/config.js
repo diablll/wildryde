d6ee8ddab8cd6653f72a81e9861b17f186944676
diablll <96254106+diablll@users.noreply.github.com>
2022-04-11 16:18:10 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
   cognito: {
-    userPoolId: "us-west-2_Gl1gpDfZt",
-    userPoolClientId: "48dnc3fu1rjsg8lifvpms7cslm",
-    region: "us-west-2"
+    userPoolId: "us-east-1_MIRpsi1Qd",
+    userPoolClientId: "3jut1ifuo1pgo67611aj37o2ds",
+    region: "us-east-1"
   },
   api: {
     invokeUrl: ""
